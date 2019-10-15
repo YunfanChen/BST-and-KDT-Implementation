81f122c38f3f6259ca57fdb026d6c6928f5949d0
Yunfan Chen <1035541313@qq.com>
2019-10-14 21:19:12 -0700

    debug

--- test/kdt/test_KDT.cpp
+++ test/kdt/test_KDT.cpp
@@ -47,6 +47,8 @@ TEST_F(SmallKDTFixture, TEST_NEAREST_POINT) {
     //naiveSearch.build(vec);
     Point queryPoint({5.81, 3.21});
     //Point* closestPoint = naiveSearch.findNearestNeighbor(queryPoint);
+    Point* closestPoint; //!!
+
     cout << "Yunfan debug... before Assertion" << endl;
     ASSERT_EQ(*kdt.findNearestNeighbor(queryPoint), *closestPoint);
     cout << "Yunfan debug... after Assertion" << endl;

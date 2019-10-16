eb710161dd0e1244b424a62eed18e4782ced3e0d
Yunfan Chen <1035541313@qq.com>
2019-10-15 22:56:01 -0700

    debug

--- src/bst/BST.hpp
+++ src/bst/BST.hpp
@@ -181,7 +181,7 @@ class BST {
       return leftH>rightH?leftH+1:rightH+1;
     }
 
-    static bool hasThisNode(BSTNode<Data>* node, BSTNode<Data>* root){
+    bool hasThisNode(BSTNode<Data>* node, BSTNode<Data>* root){
       if(this->root==nullptr) return false;
       if(root->data == node->data) return true;
       hasThisNode(node,root->left);
